c0c47927e4f4ab03e89cd32f00a8b4735cff4a41
yidafu <tiaozuwushi@qq.com>
2019-04-16 22:41:00 +0800

    Updates

---
+++ 4-fade0802bea35225405c.js
@@ -0,0 +1,15 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[4],{
+
+/***/ "./.cache/data.json":
+/*!**************************!*\
+  !*** ./.cache/data.json ***!
+  \**************************/
+/*! exports provided: pages, dataPaths, default */
+/***/ (function(module) {
+
+module.exports = {"pages":[{"componentChunkName":"component---src-pages-index-js","jsonName":"index","path":"/"},{"componentChunkName":"component---src-pages-404-js","jsonName":"404-html-516","path":"/404.html"},{"componentChunkName":"component---src-pages-404-js","jsonName":"404-22d","path":"/404/"},{"componentChunkName":"component---src-pages-about-me-jsx","jsonName":"about-me-a3a","path":"/about-me/"},{"componentChunkName":"component---src-pages-drink-jsx","jsonName":"drink-0c7","path":"/drink/"},{"componentChunkName":"component---src-templates-blog-post-jsx","jsonName":"post-2019-03-23-first-blog-f3f","path":"/post/2019-03-23-first-blog"},{"componentChunkName":"component---src-templates-main-pagination-jsx","jsonName":"posts-b4e","path":"/posts"}],"dataPaths":{"404-22d":"44/path---404-22-d-bce-NZuapzHg3X9TaN1iIixfv1W23E","404-html-516":"164/path---404-html-516-62a-NZuapzHg3X9TaN1iIixfv1W23E","about-me-a3a":"865/path---about-me-a-3-a-888-NZuapzHg3X9TaN1iIixfv1W23E","drink-0c7":"33/path---drink-0-c-7-b0d-NZuapzHg3X9TaN1iIixfv1W23E","index":"92/path---index-6a9-a2L8LtzyGrtLRqka33nawStIHRE","post-2019-03-23-first-blog-f3f":"146/path---post-2019-03-23-first-blog-f-3-f-7ce-EgDQDlM5Frqf5Gtjk8NfyH4t5Fs","posts-b4e":"689/path---posts-b-4-e-159-kIVmkD7ueVFplf34bw8dA6s5tI","sq--src-components-archives-jsx":1506232949,"sq--src-components-category-jsx":1904020567,"sq--src-components-tags-jsx":299507749}};
+
+/***/ })
+
+}]);
+//# sourceMappingURL=4-fade0802bea35225405c.js.map
