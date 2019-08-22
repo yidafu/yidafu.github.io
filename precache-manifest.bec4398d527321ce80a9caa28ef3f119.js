9ecdd2498eb3d20c9a568da5fe3ada009e095853
yidafu <yidafu@qq.com>
2019-08-22 08:23:32 +0800

    init nice-beer static asset

---
+++ precache-manifest.bec4398d527321ce80a9caa28ef3f119.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71cb514360c32822194b3e8a7c1c5a2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "853bc586e6bac35ff68f",
+    "url": "/static/css/2.1c7ffa40.chunk.css"
+  },
+  {
+    "revision": "77bd2cc2385c25de897d",
+    "url": "/static/css/main.1d9dbecb.chunk.css"
+  },
+  {
+    "revision": "853bc586e6bac35ff68f",
+    "url": "/static/js/2.713b130a.chunk.js"
+  },
+  {
+    "revision": "77bd2cc2385c25de897d",
+    "url": "/static/js/main.b28affed.chunk.js"
+  },
+  {
+    "revision": "e746b8278b56cb2dd251",
+    "url": "/static/js/runtime~main.55d7a9ec.js"
+  }
+]);
